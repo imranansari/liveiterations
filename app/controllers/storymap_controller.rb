2ec5cf0071bbff7ce4abf6d622edc72ebad32996
iansari <imran.iansari@gmail.com>
2012-01-16 18:54:42 -0500

    requirejs fixes, bootstrap menu + new activity dialog

---
+++ app/controllers/storymap_controller.rb
@@ -0,0 +1,20 @@
+class StorymapController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  end
+
+  def test
+  end
+
+  def display
+
+    puts "epic display"
+    @hellos = User.all
+
+    respond_to do |format|
+      format.json { render json: @hellos }
+    end
+  end
+
+end
