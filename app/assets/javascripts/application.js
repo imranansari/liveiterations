91617034d8f9fae0a76a6773ed9e48c854966684
iansari <imran.iansari@gmail.com>
2012-01-22 16:21:27 -0500

    heroku fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,3 @@
-require.config({
-  paths: {
-    jquery: 'libs/jquery/jquery-min',
-    rails: 'libs/jquery/rails',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-optamd3-min',
-    modelbinding: 'libs/backbone/backbone.modelbinding',
-    twDropdown: 'libs/twitter/bootstrap-dropdown',
-    twTransition: 'libs/twitter/bootstrap-transition',
-    twAlert: 'libs/twitter/bootstrap-alert',
-    twModal: 'libs/twitter/bootstrap-modal',
-    twScroll: 'libs/twitter/bootstrap-scrollspy',
-    twTab: 'libs/twitter/bootstrap-tab',
-    twTooltip1: 'libs/twitter/bootstrap-tooltip',
-    twPopover: 'libs/twitter/bootstrap-popover',
-    twButton: 'libs/twitter/bootstrap-button',
-    twCollapse: 'libs/twitter/bootstrap-collapse',
-    twCarousel: 'libs/twitter/bootstrap-carousel',
-    twTypeahead: 'libs/twitter/bootstrap-typeahead',
-    handlebars: 'libs/handlebars/handlebars.1.0.0.beta.3',
-    text: 'libs/require/text'
-  }
-
-});
-
 require(["order!jquery",
     "order!rails",
     "order!backbone",
