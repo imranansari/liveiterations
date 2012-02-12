a3f7addb4e87f3c77da3ff6e33537081ad89e067
iansari <imran.iansari@gmail.com>
2012-02-12 18:47:09 -0500

    revert

--- app/assets/javascripts/messaging_app.js
+++ app/assets/javascripts/messaging_app.js
@@ -9,46 +9,44 @@ require(["order!jquery",
 define(['underscore', 'backbone', 'views/newMessageView', 'views/messagesView', 'collections/messages', 'models/message', 'text!templates/menu.html'],
     function (_, Backbone, NewMessageView, MessagesView, Messages, Message, menuHtml) {
 
-    $(document).ready(function () {
+        $(document).ready(function () {
 
-        $("#topmenu").html(menuHtml);
+            $("#topmenu").html(menuHtml);
 
-        var newMessageView = new NewMessageView();
-        newMessageView.render();
+            var newMessageView = new NewMessageView();
+            newMessageView.render();
 
 
-/*        //var messages = new Messages();
-        Messages.add(new Message({desc:'Hello1'}));
-        Messages.add(new Message({desc:'Hello2'}));
+            /*        //var messages = new Messages();
+             Messages.add(new Message({desc:'Hello1'}));
+             Messages.add(new Message({desc:'Hello2'}));
 
-        var messagesView = new MessagesView({collection:Messages, el:$('#messages')});
-        messagesView.render();*/
+             var messagesView = new MessagesView({collection:Messages, el:$('#messages')});
+             messagesView.render();*/
 
-/*        Messages.fetch({
-            success: function(Messages) {
-                var messagesView = new MessagesView({collection:Messages, el:$('#messages')});
-                        messagesView.render();
-            }
-        });*/
-
-        var messagesView = new MessagesView({collection:Messages});
-
-        var updateMessages = function() {
             Messages.fetch({
-                success: function(Messages) {
-                    $('#messages').html("");
+                success:function (Messages) {
                     var messagesView = new MessagesView({collection:Messages, el:$('#messages')});
-                            messagesView.render();
+                    messagesView.render();
                 }
             });
-          setTimeout(updateMessages, 15000);
-        };
-        updateMessages();
+
+            // var messagesView = new MessagesView({collection:Messages});
+
+            /*        var updateMessages = function() {
+             $('#messages').html("");
+             Messages.fetch({
+             success: function(Messages) {
+             var messagesView = new MessagesView({collection:Messages, el:$('#messages')});
+             messagesView.render();
+             }
+             });
+             setTimeout(updateMessages, 5000);
+             };
+             updateMessages();*/
 
 
-
+        });
 
     });
-
-});
 
