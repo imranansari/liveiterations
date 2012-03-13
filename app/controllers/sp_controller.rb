6225186614e969280bc7daa315a8ac360e770ac6
iansari <imran.iansari@gmail.com>
2012-03-13 11:35:28 -0400

    subvert auth temporarily

--- app/controllers/sp_controller.rb
+++ app/controllers/sp_controller.rb
@@ -7,4 +7,16 @@ class SpController < ApplicationController
   def components
     render :layout => false
   end
+
+  def list
+    respond_to do |format|
+      format.json {
+        projects = Project.all.order_by([:created_at,:desc])
+
+        render json: projects
+      }
+    end
+
+  end
+
 end
